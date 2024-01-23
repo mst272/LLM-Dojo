5fbdce6149ba91fc7c95da833e3471402f6a4211
mst272 <67250532+mst272@users.noreply.github.com>
2024-01-23 22:51:04 +0800

    Delete llm_utils

---
+++ llm_utils/args.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+class CommonArgs:
+    """
+    一些常用的自定义参数
+    """
+    max_len: int = field(metadata={"help":"最大输入长度"})
+    train_data_path: Optional[str] = field(metadata={"help":"训练集路径"}
+                                           )
