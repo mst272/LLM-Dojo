b66d929ec4842ba48caa9b2fef1e29eb6666b763
mst272 <sdwzh272@163.com>
2024-04-24 18:52:45 +0800

    updata

--- llm_tricks/dora/dora_example.py
+++ llm_tricks/dora/dora_example.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
+import copy
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 BATCH_SIZE = 64
@@ -75,17 +76,111 @@ def computer_metrics(model, data_loader, device):
     correct_pred, num_examples = 0, 0
     with torch.no_grad():
         for images, labels in data_loader:
-            images = images.view(-1, 28*28).to(device)
+            # Image batch dimensions: torch.Size([64, 1, 28, 28])
+            # Image label dimensions: torch.Size([64])
+
+            images = images.view(-1, 28 * 28).to(device)
             labels = labels.to(device)
             logits = model(images)
+            _, predicted_labels = torch.max(logits, 1)
+            num_examples = labels.size(0)
+            correct_pred += (predicted_labels == labels).sum()
+            return correct_pred.float() / num_examples * 100
 
 
+# ---------------------Train---------------------------------------------
 
 
+def train(epochs, model, optimizer, train_loader, device):
+    start_time = time.time()
+    for epoch in range(epochs):
+        model.train()
+        for batch_idx, (images, labels) in enumerate(train_loader):
+            images = images.view(-1, 28 * 28).to(device)
+            labels = labels.to(device)
+
+            # forward and back
+            logits = model(images)
+            loss = F.cross_entropy(logits, labels)
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            # UPDATE MODEL PARAMETERS
+            optimizer.step()
+
+            # LOGGING
+            if not batch_idx % 400:
+                print('Epoch: %03d/%03d | Batch %03d/%03d | Loss: %.4f'
+                      % (epoch + 1, num_epochs, batch_idx,
+                         len(train_loader), loss))
+        with torch.set_grad_enabled(False):
+            print('Epoch: %03d/%03d training accuracy: %.2f%%' % (
+                epoch + 1, num_epochs,
+                computer_metrics(model, train_loader, device)))
+        print('Time elapsed: %.2f min' % ((time.time() - start_time) / 60))
+    print('Total Training Time: %.2f min' % ((time.time() - start_time) / 60))
 
 
+# ---------------------Lora Model---------------------------------------------
+class LoRALayer(nn.Module):
+    def __init__(self, in_dim, out_dim, rank, alpha):
+        super().__init__()
+        std_dev = 1 / torch.sqrt(torch.tensor(rank).float())
+        self.A = nn.Parameter(torch.rand(in_dim, rank) * std_dev)
+        self.B = nn.Parameter(torch.zeros(rank, out_dim))
+        self.alpha = alpha
+
+    def forward(self, x):
+        x = self.alpha * (x @ self.A @ self.B)
+        return x
 
 
+class LinearWithLoRA(nn.Module):
+    def __init__(self, linear, rank, alpha):
+        super().__init__()
+        self.linear = linear
+        self.lora = LoRALayer(
+            linear.in_features,
+            linear.out_features,
+            rank,
+            alpha
+        )
+
+    def forward(self, x):
+        return self.linear(x) + self.lora(x)
 
 
+# ---------------------DoRA Model---------------------------------------------
+class LinearWithDoRA(nn.Module):
+    def __init__(self, linear, rank, alpha):
+        super().__init__()
+        self.linear = linear
+        self.lora = LoRALayer(
+            linear.in_features, linear.out_features, rank, alpha
+        )
+        self.m = nn.Parameter(torch.ones(1, linear.out_features))
 
+    def forward(self, x):
+        linear_out = self.linear(x)
+        lora_out = self.lora(x)
+        lora_out_norm = lora_out / (lora_out.norm(p=2, dim=1, keepdim=True) + 1e-9)
+        dora_modification = self.m * lora_out_norm
+        return linear_out + dora_modification
+
+
+# 冻结模型的线性层，即可达到lora的只训练额外的lora层
+def freeze_linear_layers(model):
+    for child in model.children():
+        if isinstance(child, nn.Linear):
+            for param in child.parameters():
+                param.requires_grad = False
+        else:
+            # Recursively freeze linear layers in children modules
+            freeze_linear_layers(child)
+
+
+if __name__ == '__main__':
+    # 复制两份模型，以供lora 和 dora分别实验
+    model_lora = copy.deepcopy(model)
+    model_dora = copy.deepcopy(model)
