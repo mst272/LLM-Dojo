c21c2587e271a2ea24e410c358334655bbf14d64
mst272 <sdwzh272@163.com>
2024-05-17 17:59:46 +0800

    update deepspeed

--- train_args/sft/lora_qlora/base.py
+++ train_args/sft/lora_qlora/base.py
@@ -18,20 +18,22 @@ class TrainArgument(TrainingArguments):
     gradient_checkpointing: bool = field(default=True, metadata={"help": "是否使用梯度累计"})
     gradient_accumulation_steps: int = field(default=16, metadata={"help": "梯度累计的步长"})
     learning_rate: float = field(default=2e-4, metadata={"help": "学习率"})
-    logging_steps: int = field(default=500, metadata={"help": "打印的步长"})
+    logging_steps: int = field(default=100, metadata={"help": "打印的步长"})
     save_steps: int = field(default=500, metadata={"help": "多少步长保存一次"})
-    save_total_limit: Optional[int] = field(default=3, metadata={"help": "If a value is passed, will limit the total "
+    evaluation_strategy: Union[IntervalStrategy, str] = field(default="no", metadata={"help": "The evaluation "
+                                                                                                 "strategy to use."},)
+    save_strategy: Union[IntervalStrategy, str] = field(default="epoch", metadata={"help": "The checkpoint save "
+                                                                                           "strategy to use."},)
+    save_total_limit: Optional[int] = field(default=2, metadata={"help": "If a value is passed, will limit the total "
                                                                          "amount of checkpoints. Deletes the older "
                                                                          "checkpoints in"})
     lr_scheduler_type: Union[SchedulerType, str] = field(default="constant_with_warmup",
                                                          metadata={"help": "The scheduler type to use."})
-    warmup_steps: int = field(default=100, metadata={"help": "Linear warmup over warmup_steps."})
+    warmup_steps: int = field(default=10, metadata={"help": "Linear warmup over warmup_steps."})
     optim: Union[OptimizerNames, str] = field(default='paged_adamw_32bit', metadata={"help": "The optimizer to use."})
     seed: int = field(default=42, metadata={"help": "Random seed that will be set at the beginning of training."})
     report_to: Optional[List[str]] = field(default='tensorboard', metadata={
         "help": "The list of integrations to report the results and logs to."})
-    save_strategy: Union[IntervalStrategy, str] = field(default="steps",
-                                                        metadata={"help": "The checkpoint save strategy to use."})
     weight_decay: float = field(default=0.0, metadata={"help": "Weight decay for AdamW if we apply some."})
     max_grad_norm: float = field(default=1.0, metadata={"help": "Max gradient norm."})
     remove_unused_columns: Optional[bool] = field(default=False, metadata={
@@ -42,3 +44,5 @@ class TrainArgument(TrainingArguments):
                  )
         })
     fp16: bool = field(default=False, metadata={"help": "Whether to use fp16 (mixed) precision instead of 32-bit"})
+
+    deepspeed: str = '/home/nlp/wzh/LLM-Dojo/train_args/config_zero2.json'
