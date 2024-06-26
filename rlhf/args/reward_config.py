03724e37f2a7314f45020e210f5245b7e2687b6b
mst272 <sdwzh272@163.com>
2024-06-26 18:05:24 +0800

    update rlhf ing

--- rlhf/args/reward_config.py
+++ rlhf/args/reward_config.py
@@ -1,16 +1,12 @@
-from dataclasses import dataclass
-from typing import Optional
-from transformers import TrainingArguments
-
+from dataclasses import dataclass, field
+from typing import Optional, Union, List
+from transformers import TrainingArguments, SchedulerType, IntervalStrategy
+from transformers.training_args import OptimizerNames
 
 @dataclass
 class RewardConfig(TrainingArguments):
     """
     RewardConfig collects all training arguments related to the [`RewardTrainer`] class.
-
-    Using [`HfArgumentParser`] we can turn this class into
-    [argparse](https://docs.python.org/3/library/argparse#module-argparse) arguments that can be specified on the
-    command line.
 
     Parameters:
         max_length (`int`, *optional*, defaults to `None`):
@@ -21,4 +17,41 @@ class RewardConfig(TrainingArguments):
 
     max_length: Optional[int] = None
     """The maximum length of the sequences in the batch. This argument is required if you want to use the default data collator."""
-    gradient_checkpointing =
+
+    """
+        训练参数, 直接在这里修改default即可
+    """
+    output_dir: str = field(default='', metadata={"help": "模型训练完成后的保存路径"})
+    num_train_epochs: int = field(default=1, metadata={"help": "训练轮次"})
+    per_device_train_batch_size: int = field(default=2, metadata={"help": "训练的batch size"})
+    gradient_checkpointing: bool = field(default=True, metadata={"help": "是否使用梯度累计"})
+    gradient_accumulation_steps: int = field(default=16, metadata={"help": "梯度累计的步长"})
+    learning_rate: float = field(default=2e-4, metadata={"help": "学习率"})
+    logging_steps: int = field(default=100, metadata={"help": "打印的步长"})
+    save_steps: int = field(default=500, metadata={"help": "多少步长保存一次"})
+    save_strategy: Union[IntervalStrategy, str] = field(default="epoch", metadata={"help": "The checkpoint save "
+                                                                                           "strategy to use."}, )
+    save_total_limit: Optional[int] = field(default=2, metadata={"help": "If a value is passed, will limit the total "
+                                                                         "amount of checkpoints. Deletes the older "
+                                                                         "checkpoints in"})
+    lr_scheduler_type: Union[SchedulerType, str] = field(default="constant_with_warmup",
+                                                         metadata={"help": "The scheduler type to use."})
+    warmup_steps: int = field(default=10, metadata={"help": "Linear warmup over warmup_steps."})
+    optim: Union[OptimizerNames, str] = field(default='paged_adamw_32bit', metadata={"help": "The optimizer to use."})
+    seed: int = field(default=42, metadata={"help": "Random seed that will be set at the beginning of training."})
+    report_to: Optional[List[str]] = field(default='tensorboard', metadata={
+        "help": "The list of integrations to report the results and logs to."})
+    weight_decay: float = field(default=0.0, metadata={"help": "Weight decay for AdamW if we apply some."})
+    max_grad_norm: float = field(default=1.0, metadata={"help": "Max gradient norm."})
+    remove_unused_columns: Optional[bool] = field(default=False, metadata={
+        "help": "Remove columns not required by the model when using an nlp.Dataset."})
+    bf16: bool = field(default=True, metadata={
+        "help": ("Whether to use bf16 (mixed) precision instead of 32-bit. Requires Ampere or higher NVIDIA"
+                 " architecture or using CPU (use_cpu) or Ascend NPU. This is an experimental API and it may change."
+                 )
+    })
+    fp16: bool = field(default=False, metadata={"help": "Whether to use fp16 (mixed) precision instead of 32-bit"})
+
+    # Deepspeed训练相关参数，不使用时设置为default=None
+    deepspeed: Optional[str] = field(default='./train_args/deepspeed_config/ds_config_zero2.json',
+                                     metadata={"help": "启用Deepspeed时需要的config文件"})
--- rlhf/reward_model.py
+++ rlhf/reward_model.py
@@ -1,1 +1,86 @@
-from trl.trainer.utils import RewardDataCollatorWithPadding, DPODataCollatorWithPadding
+import warnings
+from datasets import load_dataset
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, HfArgumentParser
+from trl import RewardTrainer, get_kbit_device_map, get_peft_config, get_quantization_config
+from args.model_config import Model_Config
+from args.reward_config import RewardConfig
+
+parser = HfArgumentParser((RewardConfig, Model_Config))
+config, model_config = parser.parse_args_into_dataclasses()
+config.gradient_checkpointing_kwargs = dict(use_reentrant=False)
+
+################
+# Model & Tokenizer
+################
+quantization_config = get_quantization_config(model_config)
+model_kwargs = dict(
+    revision=model_config.model_revision,
+    trust_remote_code=model_config.trust_remote_code,
+    device_map=get_kbit_device_map() if quantization_config is not None else None,
+    quantization_config=quantization_config,
+)
+tokenizer = AutoTokenizer.from_pretrained(model_config.model_name_or_path, use_fast=True)
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_config.model_name_or_path, num_labels=1, **model_kwargs
+)
+
+if model_config.lora_task_type != "SEQ_CLS":
+    warnings.warn(
+        "You are using a `task_type` that is different than `SEQ_CLS` for PEFT. This will lead to silent bugs"
+        " Make sure to pass --lora_task_type SEQ_CLS when using this script."
+    )
+
+################
+# Dataset
+################
+raw_datasets = load_dataset("Anthropic/hh-rlhf")
+# Tokenize chosen/rejected pairs of inputs
+# Adapt this section to your needs for custom datasets
+
+def preprocess_function(examples):
+    new_examples = {
+        "input_ids_chosen": [],
+        "attention_mask_chosen": [],
+        "input_ids_rejected": [],
+        "attention_mask_rejected": [],
+    }
+    for chosen, rejected in zip(examples["chosen"], examples["rejected"]):
+        tokenized_chosen = tokenizer(chosen)
+        tokenized_rejected = tokenizer(rejected)
+
+        new_examples["input_ids_chosen"].append(tokenized_chosen["input_ids"])
+        new_examples["attention_mask_chosen"].append(tokenized_chosen["attention_mask"])
+        new_examples["input_ids_rejected"].append(tokenized_rejected["input_ids"])
+        new_examples["attention_mask_rejected"].append(tokenized_rejected["attention_mask"])
+
+    return new_examples
+
+# Preprocess the dataset and filter out examples that are longer than args.max_length
+raw_datasets = raw_datasets.map(
+    preprocess_function,
+    batched=True,
+    num_proc=4,
+)
+raw_datasets = raw_datasets.filter(
+    lambda x: len(x["input_ids_chosen"]) <= config.max_length and len(x["input_ids_rejected"]) <= config.max_length
+)
+train_dataset = raw_datasets["train"]
+eval_dataset = raw_datasets["test"]
+
+################
+# Training
+################
+trainer = RewardTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=config,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    peft_config=get_peft_config(model_config),
+)
+trainer.train()
+trainer.save_model(config.output_dir)
+trainer.push_to_hub()
+metrics = trainer.evaluate()
+trainer.log_metrics("eval", metrics)
+print(metrics)
