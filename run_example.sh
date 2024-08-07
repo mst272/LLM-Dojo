050ea09f21622c258f6bdce8b74b45b9e7b9558e
mst272 <67250532+mst272@users.noreply.github.com>
2024-08-07 17:40:14 +0800

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,7 +1,10 @@
 deepspeed --include localhost:0,1 main_train.py\
     --train_data_path 数据集路径\
     --model_name_or_path  模型路径\
+    --task_type sft\
     --train_mode qlora\
     --output_dir 输出路径
+
+# task_type:[pretrain, sft, dpo_multi, dpo_single]
 
 # python main_train.py --train_data_path 数据集路径 --model_name_or_path 模型路径
