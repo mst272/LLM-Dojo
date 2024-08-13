08e39f84a871cbe4b9801f7946164369ee1d9b4d
mst272 <sdwzh272@163.com>
2024-08-13 20:56:46 +0800

    update dpo

--- llm_tricks/DPO_example/dpo.py
+++ llm_tricks/DPO_example/dpo.py
@@ -36,6 +36,8 @@ def compute_logprobs(logits, labels, mask=None):
 
 def compute_batch_loss(batch, policy_model, reference_model, beta):
     """Compute the DPO loss on an input batch"""
+    loss_fn = DPOLoss(beta)
+
     policy_chosen_logps = compute_logprobs(
         logits=policy_model(batch["chosen"]),
         labels=batch["chosen"],
@@ -46,6 +48,55 @@ def compute_batch_loss(batch, policy_model, reference_model, beta):
         labels=batch["rejected"],
         mask=batch["rejected_mask"]
     )
+    reference_chosen_logps = compute_logprobs(
+        logits=reference_model(batch['chosen']),
+        labels=batch['chosen'],
+        mask=batch["chosen_mask"]
+    )
+    reference_rejected_logps = compute_logprobs(
+        logits=reference_model(batch['rejected']),
+        labels=batch['rejected'],
+        mask=batch["rejected_mask"]
+    )
+    loss, chosen_rewards, rejected_rewards = loss_fn(
+        policy_chosen_logps=policy_chosen_logps,
+        policy_rejected_logps=policy_rejected_logps,
+        reference_chosen_logps=reference_chosen_logps,
+        reference_rejected_logps=reference_rejected_logps,
+        beta=beta
+    )
+    return loss, chosen_rewards, rejected_rewards
+
+
+def compute_dataloader_loss(data_loader, policy_model, reference_model, beta, num_batch=None):
+    total_loss, total_chosen_rewards, total_rejected_rewards = 0., 0., 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batch is None:
+        num_batches = len(data_loader)
+    else:
+        # Reduce the number of batches to match the total number of batches in the data loader
+        # if num_batches exceeds the number of batches in the data loader
+        num_batches = min(num_batch, len(data_loader))
+
+    for i, batch in enumerate(data_loader):
+        if i < num_batches:
+            loss, chosen_rewards, rejected_rewards = compute_batch_loss(
+                batch=batch,
+                policy_model=policy_model,
+                reference_model=reference_model,
+                beta=beta
+            )
+            total_loss += loss.item()
+            total_chosen_rewards += chosen_rewards.item()
+            total_rejected_rewards += rejected_rewards.item()
+
+        else:
+            break
+    total_loss /= num_batches
+    total_chosen_rewards /= num_batches
+    total_rejected_rewards /= num_batches
+    return total_loss, total_chosen_rewards, total_rejected_rewards
 
 
 class DPOLoss(nn.Module):
