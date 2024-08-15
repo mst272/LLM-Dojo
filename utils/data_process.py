2be51892236731a47e6649f66199227fba60ff63
mst272 <sdwzh272@163.com>
2024-08-15 15:23:03 +0800

    fix bugs and update dpo example

--- utils/data_process.py
+++ utils/data_process.py
@@ -3,72 +3,6 @@ from torch.utils.data import Dataset
 from loguru import logger
 
 
-# class CommonSingleRoundDataProcess(Dataset):
-#     """
-#     默认使用的单轮对话常规dataset
-#     """
-#
-#     def __init__(self, file, tokenizer, max_length, template):
-#         self.tokenizer = tokenizer
-#         self.template_name = template.template_name
-#         self.system_format = template.system_format
-#         self.user_format = template.user_format
-#         self.assistant_format = template.assistant_format
-#         self.system = template.system
-#
-#         self.max_length = max_length
-#
-#         logger.info(f'Loading data: {file}')
-#         with open(file, 'r', encoding='utf8') as f:
-#             data_list = f.readlines()
-#         logger.info(f'Use template "{self.template_name}" for training')
-#         logger.info(f"There are {len(data_list)} data in dataset")
-#         self.data_list = data_list
-#
-#     def __len__(self):
-#         return len(self.data_list)
-#
-#     def __getitem__(self, item):
-#         # 开始拼接每条数据
-#         data = self.data_list[item]
-#         data = json.loads(data)
-#         input_ids, target_mask = [], []
-#
-#         if self.system_format is not None:
-#             system = self.system
-#             if system is not None:
-#                 system_text = self.system_format.format(content=system)
-#                 input_ids = self.tokenizer.encode(system_text, add_special_tokens=False)
-#                 target_mask = [0] * len(input_ids)
-#         instruction = data['instruction']
-#         output = data['output']
-#
-#         instruction_text = self.user_format.format(content=instruction, stop_token=self.tokenizer.eos_token)
-#         output_text = self.assistant_format.format(content=output, stop_token=self.tokenizer.eos_token)
-#
-#         input_tokens = self.tokenizer.encode(instruction_text, add_special_tokens=False)
-#         output_tokens = self.tokenizer.encode(output_text, add_special_tokens=False)
-#
-#         input_ids += input_tokens + output_tokens
-#         target_mask += [0] * len(input_tokens) + [1] * len(output_tokens)
-#
-#         # 判断一下输入和掩码长度是否相等
-#         assert len(input_ids) == len(target_mask)
-#
-#         # 对长度进行截断
-#         input_ids = input_ids[:self.max_length]
-#         target_mask = target_mask[:self.max_length]
-#         attention_mask = [1] * len(input_ids)
-#         assert len(input_ids) == len(target_mask) == len(attention_mask)
-#         inputs = {
-#             "input_ids": input_ids,
-#             "attention_mask": attention_mask,
-#             "target_mask": target_mask
-#         }
-#
-#         return inputs
-#
-
 class MultiRoundDataProcess(Dataset):
     def __init__(self, file, tokenizer, max_length):
         self.tokenizer = tokenizer
@@ -90,7 +24,8 @@ class MultiRoundDataProcess(Dataset):
 
         if data['message'][0]['role'] == 'system':
             system_text = [data['message'][0]]
-            system_text = self.tokenizer.apply_chat_template(system_text, tokenize=False, add_generation_prompt=False, return_tensors="pt")
+            system_text = self.tokenizer.apply_chat_template(system_text, tokenize=False, add_generation_prompt=False,
+                                                             return_tensors="pt")
             input_ids = self.tokenizer.encode(system_text, add_special_tokens=False)
             target_mask = [0] * len(input_ids)
 
@@ -98,12 +33,14 @@ class MultiRoundDataProcess(Dataset):
         # 拼接多轮对话
         for i, conv in enumerate(message):
             if conv['role'] == 'user':
-                user_text = self.tokenizer.apply_chat_template([conv], tokenize=False, add_generation_prompt=False, return_tensors="pt")
+                user_text = self.tokenizer.apply_chat_template([conv], tokenize=False, add_generation_prompt=False,
+                                                               return_tensors="pt")
                 input_tokens = self.tokenizer.encode(user_text, add_special_tokens=False)
                 input_ids += input_tokens
                 target_mask += [0] * len(input_tokens)
             elif conv['role'] == 'assistant':
-                assistant_text = self.tokenizer.apply_chat_template([conv], tokenize=False, add_generation_prompt=False,return_tensors="pt")
+                assistant_text = self.tokenizer.apply_chat_template([conv], tokenize=False, add_generation_prompt=False,
+                                                                    return_tensors="pt")
                 output_tokens = self.tokenizer.encode(assistant_text, add_special_tokens=False)
                 input_ids += output_tokens
                 target_mask += [1] * len(output_tokens)
