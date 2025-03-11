c417909ea6f6defdb2f29416640961653c2946c2
mst272 <sdwzh272@163.com>
2025-03-12 01:52:53 +0800

    update callback

--- utils/eval/generation_config.py
+++ utils/eval/generation_config.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
+from transformers import GenerationConfig
 
 
 @dataclass
-class GenerationConfig:
+class GenerationConfig(GenerationConfig):
     max_new_tokens: int = 256
     max_length: int = 512
     num_beams: int = 1
     temperature: float = 1.0
     top_p: float = 1.0
-    do_sample: bool = False
+    do_sample: bool = False,
+    use_cache: bool = False  # if gather_deepspeed3_params=False, the use_cache should be false
