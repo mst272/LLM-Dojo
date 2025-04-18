48a7de9d55ce18b030e751511d24e1f461d98b67
mst272 <sdwzh272@163.com>
2025-04-19 01:18:16 +0800

    fix bugs

--- rlhf/rejected_sampling/run_generate.sh
+++ rlhf/rejected_sampling/run_generate.sh
@@ -1,6 +1,6 @@
-dataset_name='/workspace/mnt/cmss-wzh/0317_no_sys_data.jsonl'
-model_name_or_path='/workspace/alg/c932e29f-31eb-478f-a50f-1262e98adf05/cmss-wangxuejiao/LLaMA-Factory-main/saves/qwen32b/full/Qwen2.5-Coder-32B-Instruct_1223_wad_20250317_reason_sq_8k_gb256_1e-5/checkpoint-200'
-save_filename='./0317_no_sys_data/0317_rejected_generate'
+dataset_name=''
+model_name_or_path=''
+save_filename='rejected_generate'
 
 nohup python generate.py \
     --dataset_name "$dataset_name" \
