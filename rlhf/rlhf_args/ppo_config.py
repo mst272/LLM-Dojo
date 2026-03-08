86fa01d2c56986cfddf2160f481c406689aec7ad
mst272 <sdwzh272@163.com>
2026-03-08 12:58:40 +0800

    build openrlhf-kd upon the openrlhf base and simplified the openrlhf architecture

--- rlhf/rlhf_args/ppo_config.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from trl import PPOConfig as TrlPPOConfig
-from rlhf_args.base_config import BaseConfig
-
-
-@dataclass
-class PPOConfig(BaseConfig, TrlPPOConfig):
-    eval_samples = 30 # eval数量
--- rlhf/rlhf_args/reward_config.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from rlhf_args.base_config import BaseConfig
-from trl import RewardConfig as TrlRewardConfig
-
-
-@dataclass
-class RewardConfig(BaseConfig, TrlRewardConfig):
-    pass
